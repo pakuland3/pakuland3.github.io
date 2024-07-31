4918c93ce6ab4cafc80154416e97f39fc22235a8
pakuland3 <pakuland_1@naver.com>
2024-07-31 23:34:47 +0900

    0731

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const addButton=document.getElementById("add_text");
+    addButton.addEventListener("click", function(){
+        const add_text=document.getElementById("input_text").value;
+        const logs=document.getElementById("logs");
+        
+    });
+});
